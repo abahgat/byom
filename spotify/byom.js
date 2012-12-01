9a1a57dce01dc203f531f675434d2ddc21505b25
Mattia Tommasone <tommasone@klan.it>
2012-12-01 21:04:25 +0100

    some stuff

--- spotify/byom.js
+++ spotify/byom.js
@@ -1,8 +1,14 @@
 
 var Byom = function() {
+	var api_base_url = 'http://api.usergrid.com/diderikvw/byom/';
+	var jukebox_uuid = 'b29c4d6f-3bea-11e2-9141-02e81ae640dc';
+	
 	var playlist = [];
-	var playingSong = null;
-	var poll;
+	var playingSong = null;	
+	var swipes_poll;
+	var latest_swipes_poll = 0;
+	var jukebox_poll;
+	var latest_jukebox_poll = 0;
 			
 	this.updatePlaylist = function() {
 		console.log(playlist);
@@ -17,7 +23,7 @@ var Byom = function() {
 	}
 
 	this.songFinished = function() {
-		var nextSong = playlist.splice(0, 1);
+		var nextSong = computeNextSong();
 		if(nextSong.length > 0) {
 			playingSong = nextSong[0];
 			$('#playing-title').html(playingSong.title);
@@ -34,35 +40,32 @@ var Byom = function() {
 
 
 	this.getJukebox = function() {
-		$.getJSON('http://api.usergrid.com/diderikvw/byom/jukeboxes/fd3447c9-3bd9-11e2-9141-02e81ae640dc', function(data) {
+		$.getJSON('http://api.usergrid.com/diderikvw/byom/jukeboxes/', function(data) {
 			console.log(data);
-			$.each(data.entities[0].playlists, function(index, value) {				
-				console.log('playlist ' + value);
-				models.Playlist.fromURI(value).load('name', 'tracks').done(function(spotifyPlaylist) {
-					console.log('Playlist ' + JSON.stringify(spotifyPlaylist));
-					lastplaylist = spotifyPlaylist;
-					spotifyPlaylist.tracks.snapshot().done(function(tracks) {
-						for(var i = 0; i < tracks.length; i++) {
-							console.log('Track ' + JSON.stringify(tracks.get(i)));
-							lasttrack = tracks.get(i);
-							playlist.push(tracks.get(i));
-						}
-					});
-				  	
-				  	
-				});
-
-			});
-			byom.updatePlaylist();
+			if(data.entities[0].modified > latest_jukebox_poll) {
+				$.each(data.entities[0].playlists, function(index, value) {				
+					console.log('playlist ' + value);
+					byom.addSpotifyPlaylist(value);
+				});			
+			}
+			latest_jukebox_poll = data.entities[0].modified;
 		})
 	}
 
 	this.startSwipesPoll = function() {
-		poll = setInterval(getSwipes, 5000);
+		swipes_poll = setInterval(getSwipes, 5000);
 	}
 
 	this.stopSwipesPoll = function() {
-		clearInterval(poll);
+		clearInterval(swipes_poll);
+	}
+
+	this.startJukeboxPoll = function() {
+		jukebox_poll = setInterval(this.getJukebox, 5000);
+	}
+
+	this.stopJukeboxPoll = function() {
+		clearInterval(jukebox_poll);
 	}
 
 	this.getPlaylist = function() {
@@ -70,30 +73,95 @@ var Byom = function() {
 	}
 
 	this.getCards = function() {
-		$.getJSON('http://api.usergrid.com/diderikvw/byom/cards', function(data) {
+		$.getJSON(api_base_url + 'cards', function(data) {
 			console.log(data);
 		});
 	}
 
-	var getSwipes = function() {
-		
-		$.getJSON('http://api.usergrid.com/diderikvw/byom/swipes/', function(data) {
+	var getSwipes = function() {		
+		console.log(latest_swipes_poll);
+		$.getJSON(api_base_url + 'swipes/', function(data) {
+			console.log(data);
 			$.each(data.entities, function(index, value) {
-
-			});
+				$.getJSON(api_base_url + "cards?filter=uid%3D'" + value.carduid + "'", function(card_data) {							
+					if(typeof(card_data.entities) == 'object' && card_data.entities.length > 0 && card_data.entities[0].modified > latest_swipes_poll) {
+						//byom.addSpotifyPlaylist(card_data.entities[0].playlist);
+						byom.addPlayListToJukebox(card_data.entities[0].playlist);												
+					}
+					latest_poll = data.timestamp;
+				});
+			});			
 		})
 		//For each swipe, get the spotify playlist id for the card and delete the swipe
 
 	}
-
-	var getSpotifyPlaylist = function() {
-		//Get a spotify playlist from apigee
-		//For each track in the playlist, add it to the playlist var
+	
+	this.addPlayListToJukebox = function(playlistURI) {
+		$.getJSON(api_base_url + '/jukeboxes/' + jukebox_uuid, function(data) {
+			var playlists = data.entities[0].playlists;			
+			var found = false;
+			for(i in playlists) {
+				if(playlists[i] == playlistURI) {
+					found = true;
+					break;
+				}
+			}
+			if(!found) {
+				playlists.push(playlistURI);
+			}
+			console.log(playlists);
+			$.ajax({
+				type: 'PUT',
+				url: api_base_url + 'jukeboxes/' + jukebox_uuid,
+				dataType: 'json',
+				data: JSON.stringify({"playlists": playlists}),
+				processData: false,
+				contentType: 'application/json'				
+			});
+		});
 	}
 
-	var buildSongLi = function(song) {
+	this.addSpotifyPlaylist = function(playlistURI) {				
+		models.Playlist.fromURI(playlistURI).load('name', 'tracks', 'owner').done(function(spotifyPlaylist) {			
+			lastplaylist = spotifyPlaylist;
+			models.User.fromURI(spotifyPlaylist.owner).load().done(function(user) {
+				spotifyPlaylist.tracks.snapshot().done(function(tracks) {
+					for(var i = 0; i < tracks.length; i++) {						
+						var lasttrack = tracks.get(i);												
+						if(playlist[lasttrack.uri] != undefined) {
+							playlist[lasttrack.uri].owners.push[user];
+						} else {
+							lasttrack.owners = [user];
+							playlist[lasttrack.uri] = lasttrack;
+						}						
+					}
+					byom.updatePlaylist();
+				});
+			});			
+		});		
+	}
+
+	var computeNextSong = function() {
+		var candidate_next = null;
+		for(i in playlist) {
+			if(playlist.hasOwnProperty(i)) {
+				if(candidate_next == null || candidate_next.owners.length < playlist[i].owners.length) {
+					candidate_next = playlist[i];
+				}
+			}
+		}
+		return candidate_next;
+	}
+
+	var buildSongLi = function(song) {	
+		console.log(song);
 		//Probably ask spotify for the whole song data from the song id
-		var ret = '<li style="display: none"><span class="song-title">' + song.name + '</span><span class="song-artist">' + song.artists[0].name + '<span><ul class="song-users">';
+		var ret =  '<li style="display: none"><span class="song-title">' + song.name + '</span>';
+		if(typeof(song.artists) != undefined && song.artists.length <= 0) {
+			return;
+		} else {
+			ret += '<span class="song-artist">' + song.artists[0].name + '<span><ul class="song-users">';
+		}
 
 		/*$.each(song.users, function(index, value) {						
 			ret += '<li class="user">' + value + '</li>';
